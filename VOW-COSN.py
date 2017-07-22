345fcf6b1eeffe68ce7db816a6ca2d794d8165b0
biswanathptr24 <biswanath_manojkumar@srmuniv.edu.in>
2017-07-22 10:57:08 +0530

    Add files via upload

---
+++ VOW-COSN.py
@@ -0,0 +1,9 @@
+# your code goes here
+l = input("Input a letter of the alphabet: ")
+
+if l in ('a', 'e', 'i', 'o', 'u'):
+	print("%s is a vowel." % l)
+
+else:
+	print("%s is a consonant." % l) 
+	
